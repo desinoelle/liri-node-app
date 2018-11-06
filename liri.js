3478924cb8720e09f02af43d75598c63d19a3f4d
Des <desinoelle@gmail.com>
2018-11-05 19:38:42 -0500

    Added npm node modules.

--- liri.js
+++ liri.js
@@ -2,24 +2,21 @@
 require("dotenv").config();
 
 // Access to keys
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 
 // Get user command
 var command = process.argv[2];
 
 // Liri commands:
 if (command === "concert-this") {
-
+    console.log("Finding concert...");
 }
 if (command === "spotify-this-song") {
-
+    console.log("Finding song...");
 }
 if (command === "movie-this") {
-
+    console.log("Finding movie...");
 }
 if (command === "do-what-it-says") {
-
-}
-else {
-    console.log("Invalid command.");
+    console.log("Reading file...");
 }
