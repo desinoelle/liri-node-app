bcb11918b7663263389c260f9dbe6759fa883ded
Des <desinoelle@gmail.com>
2018-11-05 20:30:04 -0500

    All API keys received.

--- keys.js
+++ keys.js
@@ -4,3 +4,7 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.ombd = process.env.OMBD_ID;
+
+exports.bit = process.env.BIT_ID;
--- liri.js
+++ liri.js
@@ -1,11 +1,19 @@
-// Read dotenv
+// Require these
 require("dotenv").config();
+var Spotify = require("node-spotify-api");
+var request = require("request");
+var moment = require("moment");
+var fs = require("fs");
+var keys = require("./keys.js");
 
 // Access to keys
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
+var ombdID = keys.ombd;
+var bitID = keys.bit;
 
 // Get user command
 var command = process.argv[2];
+var userSearch = process.argv[3];
 
 // Liri commands:
 if (command === "concert-this") {
